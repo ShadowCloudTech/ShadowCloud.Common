b3f7feb895baf77761549c760aacdb26ced53072
jianjustin <janine.jian.chen@gmail.com>
2020-08-16 12:21:22 +0800

    创建基于Kotlin的应用

---
+++ src/test/kotlin/shadow/cloud/tech/common/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package shadow.cloud.tech.common
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
